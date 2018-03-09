ed87a0e213d389350e2619e59c860d0a4cd7733b
jiaweini <jiaweini@users.noreply.github.com>
2018-03-09 17:48:26 -0500

    Add files via upload

---
+++ testb/g99_MAC_tb.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use STD.textio.all;
+use ieee.std_logic_textio.all;
+
+entit g99_MAC_tb is
+end g99_MAC_tb;
+
+architecture test of g99_MAC_tb is
+component g99_MAC is
+port (x		: in	std_logic_vector(down to);
+
+
+
+)
+
+
+
+
+begin
+end architecture test;
